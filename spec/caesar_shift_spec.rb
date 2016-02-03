bf4357f7d4b1e912260d362ebc1a9afc663a2da5
Braden <braden@brazenbraden.com>
2016-02-03 22:52:29 +0200

    Adds tests and code.

--- spec/caesar_shift_spec.rb
+++ spec/caesar_shift_spec.rb
@@ -18,13 +18,41 @@ describe CaesarShift do
       caesar = CaesarShift.new(13)
       expect(caesar.rot).to eql(13)
     end
+
   end
 
+  context "encrypting a string" do
 
-  # context "encrypting a string" do
-  #   it "does a basic encrypt" do
-  #
-  #   end
-  # end
+    it "throws an exception if no string is passed" do
+      expect{CaesarShift.new().encrypt}.to raise_error(ArgumentError)
+    end
+
+    it "encrypts the alphabet using default rot of 6" do
+      cypher = CaesarShift.new().encrypt("abcdefghijklmnopqrstuvwxyz")
+      expect(cypher).to eql("ghijklmnopqrstuvwxyzabcdef")
+    end
+
+    it "encrypts 'Hello World!' using rot of 13" do
+      cypher = CaesarShift.new(13).encrypt("Hello World!")
+      expect(cypher).to eql("Uryyb Jbeyq!")
+    end
+
+  end
+
+  context "decrypts a string" do
+    it "throws an exception if no string is passed" do
+      expect{CaesarShift.new().decrypt}.to raise_error(ArgumentError)
+    end
+
+    it "decrypts the alphabet using default rot of 6" do
+      cypher = CaesarShift.new().decrypt("ghijklmnopqrstuvwxyzabcdef")
+      expect(cypher).to eql("abcdefghijklmnopqrstuvwxyz")
+    end
+
+    it "decrypts 'Uryyb Jbeyq!' using rot of 13" do
+      cypher = CaesarShift.new(13).decrypt("Uryyb Jbeyq!")
+      expect(cypher).to eql("Hello World!")
+    end
+  end
 
 end
