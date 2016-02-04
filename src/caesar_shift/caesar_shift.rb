adf011a530065cee7278e47b78f220116257a5d7
Paul Schwarz <paulsschwarz@gmail.com>
2016-02-04 09:18:01 +0300

    Removes calls to Range since we don't actually need the range, just the limits.

--- src/caesar_shift/caesar_shift.rb
+++ src/caesar_shift/caesar_shift.rb
@@ -1,10 +1,11 @@
 class CaesarShift
+  ALPHABET_SIZE = 26
+  FIRST_UPPER = 65 # A
+  FIRST_LOWER = 97 # a
   attr_accessor :rot
 
   def initialize rot = 6
     @rot = rot
-    @upper = Range.new(65, 90) # 65 - 90 = A - Z
-    @lower = Range.new(97, 122) # 97 - 122 = a - z
   end
 
   def encrypt str
@@ -19,10 +20,10 @@ class CaesarShift
 
   def rotate_str(str, offset)
     str.bytes.map {|char_code|
-      if @upper.include? char_code
-        char_code = (char_code - @upper.first + offset) % @upper.count + @upper.first
-      elsif @lower.include? char_code
-        char_code = (char_code - @lower.first + offset) % @lower.count + @lower.first
+      if char_code.between?(FIRST_UPPER, FIRST_UPPER + ALPHABET_SIZE)
+        char_code = (char_code - FIRST_UPPER + offset) % ALPHABET_SIZE + FIRST_UPPER
+      elsif char_code.between?(FIRST_LOWER, FIRST_LOWER + ALPHABET_SIZE)
+        char_code = (char_code - FIRST_LOWER + offset) % ALPHABET_SIZE + FIRST_LOWER
       end
       char_code.chr
     }.join
